e43def5aabdc2338b82f7d2687c5ab7415b89317
yataroots <yata.rootszero@gmail.com>
2015-02-25 18:27:03 -0800

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,14 @@
+# -*- coding: utf-8 -*-
 class SessionsController < ApplicationController
   def new
   end
 
   def create
-   user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ユーザーをサインインさせ、ユーザーページ (show) にリダイレクトする。
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-sign_out
+    sign_out
     redirect_to root_url
   end
 end
